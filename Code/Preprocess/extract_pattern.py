4995aff8f91bc1edecc0136c0216cd6f5ca6b476
Janzen Liu <chengtsung_liu@yahoo.com>
2017-08-27 15:33:54 +0800

    add output message while extracting pattern for debug use

--- Code/Preprocess/extract_pattern.py
+++ Code/Preprocess/extract_pattern.py
@@ -10,18 +10,14 @@ from pattern_dict import patterns
 
 ## extract single pattern 
 def extract_pattern(name, pattern, path):
+	print("Extracting pattern %s from the entire training set" % name)
 	res_train = {row['ID']:re.findall(pattern, row['Text']) for index,row in df_train_txt.iterrows()}
-	# res_train = {}
-	# for index, row in df_train_txt.iterrows():
-	# 	res_train[str(row['ID'])] = re.findall(pattern, row['Text'])
 	filename  = "%s/%s.train.json" % (path, name)
 	with open(filename, 'w') as f:
 		json.dump(res_train, f, indent=2)
 
+	print("Extracting pattern %s from the entire testing set" % name)
 	res_test = {row['ID']:re.findall(pattern, row['Text']) for index,row in df_test_txt.iterrows()}
-	# res_test = {}
-	# for index, row in df_test_txt.iterrows():
-	# 	res_test[str(row['ID'])] = re.findall(pattern, row['Text'])
 	filename  = "%s/%s.test.json" % (path, name)
 	with open(filename, 'w') as f:
 		json.dump(res_test, f, indent=2)
