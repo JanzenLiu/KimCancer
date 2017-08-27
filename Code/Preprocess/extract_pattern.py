b84096b07b3fd11734ad5ddba4fa8018d3e22f55
Janzen Liu <chengtsung_liu@yahoo.com>
2017-08-27 21:05:03 +0800

    add pattern removal for extract_pattern

--- Code/Preprocess/extract_pattern.py
+++ Code/Preprocess/extract_pattern.py
@@ -6,23 +6,44 @@ import sys
 sys.path.append("../")
 from param_config import config
 from reader import df_train_txt, df_test_txt
-from pattern_dict import patterns
+from pattern_dict import patterns, other_patterns
+
+df_train_txt_copy = df_train_txt.copy()
+df_test_txt_copy = df_test_txt.copy()
 
 ## extract single pattern 
-def extract_pattern(name, pattern, path):
+def extract_pattern(name, pattern, path, remove_match=False):
 	print("Extracting pattern %s from the entire training set..." % name)
-	res_train = {row['ID']:re.findall(pattern, row['Text']) for index,row in df_train_txt.iterrows()}
+	# res_train = {row['ID']:re.findall(pattern, row['Text']) for index,row in df_train_txt.iterrows()}
+	res_train = {}
+	for index, row in df_train_txt_copy.iterrows():
+		res_train[row['ID']] = re.findall(pattern, row['Text'])
+		if remove_match:
+			re.sub(pattern, "", row['Text'])
 	filename  = "%s/%s.train.json" % (path, name)
 	with open(filename, 'w') as f:
 		json.dump(res_train, f, indent=2)
 
 	print("Extracting pattern %s from the entire testing set..." % name)
-	res_test = {row['ID']:re.findall(pattern, row['Text']) for index,row in df_test_txt.iterrows()}
+	# res_test = {row['ID']:re.findall(pattern, row['Text']) for index,row in df_test_txt.iterrows()}
+	res_test = {}
+	for index, row in df_test_txt_copy.iterrows():
+		res_test[row['ID']] = re.findall(pattern, row['Text'])
+		if remove_match:
+			re.sub(pattern, "", row['Text'])
 	filename  = "%s/%s.test.json" % (path, name)
 	with open(filename, 'w') as f:
 		json.dump(res_test, f, indent=2)
 
-def extract_all():
+def extract_all(remove_match=False):
 	for key, value in patterns.items():
 		if not value is None:
-			extract_pattern(key, value, config.pattern_folder)
+			extract_pattern(key, value, config.pattern_folder, remove_match)
+	for key,value in other_patterns.items():
+		if not value is None:
+			extract_pattern(key, value, config.pattern_folder, remove_match)
+	if remove_match:
+		output_train_txt_file = "%s/training_text.processed" % config.data_folder
+		output_test_txt_file = "%s/test_text.processed" % config.data_folder
+		df_train_txt_copy.to_csv(output_train_txt_file, sep="\|\|")
+		df_test_txt_copy.to_csv(output_test_txt_file, sep="\|\|")
--- Code/Preprocess/pattern_dict.py
+++ Code/Preprocess/pattern_dict.py
@@ -1,6 +1,6 @@
 import re
 
-word = r"^[A-Za-z]+$"
+word_pattern = r"\b[A-Za-z]+\b"
 
 patterns = {
 	## parenthesis
@@ -11,22 +11,26 @@ patterns = {
 	"order_alpha_parenthesis": r"(?<= )\([A-Za-z]\)", # (A), (B), (a)
 	"cite_year_parenthesis": r"(?<= )\(\d{4}\)", # (1999), (2002)
 	"cite_name_year_parenthesis": r"\([^)]+, \d{4}\)", # (Youngs et al., 2011), (Slade et al., 2011; Doros et al., 2012)
-	"http(s)_parenthesis": r"\(https?.+?\)", # (http://www.hgmd.cf.ac.uk/ac/index.php)
-	"other_parenthesis": r"\([^)]+\)", # (...)
+	"url_http_parenthesis": r"\(https?.+?\)", # (http://www.hgmd.cf.ac.uk/ac/index.php)
 	## square brackets
 	"figure_square_brackets": r"\[[Ff]ig(?:ure)?\.? \w+\]", # [fig 1], [fig. 1], [Fig. 1], [Figure 1], [Figure. 1], [Figure. 1A]
 	"table_square_brackets": r"\[[Tt]able \w+\]", # [Table 1], [Table S2]
 	"cite_index_square_brackets": r"\[(?:\d+, )*\d{1,3}\]", # [1], [1, 2], [2, 5, 10]
 	"cite_index_range_square_brackets": r"\[\d+\-\d+\]", # [2-5], [10-200]
-	"cite_year_parenthesis": r"\[\d{4}\]", # [1999], [2002]
+	"cite_year_brackets": r"\[\d{4}\]", # [1999], [2002]
 	"cite_name_year_square_brackets": r"\[[^\]]+, \d{4}\]", # [Arkenbout et al., 2002], [Fernandez et al., 2000; Murphy et al., 2001]
-	"http(s)_square_brackets": r"\[https?.+?\]", # [http://projects.tcag.ca/variation/]
-	"other_square_brackets": r"\[[^\]]+\]", # [...]
+	"url_http_square_brackets": r"\[https?.+?\]", # [http://projects.tcag.ca/variation/]
 	## curly brackets
-	"other_curly_brackets": r"\{[^}]+\}", # {...}
+	# to be added...
 	## others
 	"variation": r"(?<=\W)[A-Z]\d{2,3}[A-Z*](?=\W)", # E452K, P90K, R358*
 	"ph_value": r"[Pp][Hh] \d+(?:\.\d{1,2})?", # ph 8, pH 8, PH 8, pH 8.0, pH 8.23, pH 10
 	"gene_seq": r"5[′']?\-[^′'a-z]*[AGCTU]+[^′'a-z]*\-3[′']?", # 5′-CCGGGATGACCGGAGCACCTG-3′, 5′-M13-GGCCGATTCGACCTCTCT-3′
-	"http(s)_all": r"https?.+?(?:[ ,)\]])", # http://pymol.org, http://www.olympusfluoview.com/java/colocalization/index.html
+	"url_http_plain": r"(?<![\)\]])https?.+?(?:[ ,])" # http://pymol.org, http://www.olympusfluoview.com/java/colocalization/index.html
+}
+
+other_patterns = {
+	"other_parenthesis_pattern": r"\([^)]+\)", # (...)
+	"other_brackets_pattern": r"\[[^\]]+\]", # [...]
+	"other_curly_brackets_pattern": r"\{[^}]+\}" # {...}
 }
