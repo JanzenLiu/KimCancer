868ed9046373dde71b190c0c410f306cd2836010
Janzen Liu <chengtsung_liu@yahoo.com>
2017-09-02 12:18:51 +0800

    add replace_unicode function

---
+++ Code/Preprocess/replace_text.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import re
+import pickle
+import sys; sys.path.append("../")
+from param_config import config
+from reader import load_original_text
+from unicode_dict import unicodes
+
+df_train_txt, df_test_txt = load_original_text()
+
+def replace_unicode(code_regex, new_code):
+	print("Replacing unicode %s in the entire training set..." % new_code)
+	for index, row in df_train_txt.iterrows():
+		df_train_txt.loc[index]["Text"] = re.sub(code_regex, new_code, row["Text"], re.UNICODE)
+	print("Replacing unicode %s in the entire testing set..." % new_code)
+	for index, row in df_test_txt.iterrows():
+		df_test_txt.loc[index]["Text"] = re.sub(code_regex, new_code, row["Text"], re.UNICODE)
+
+def replace_all():
+	for key, value in unicodes.items():
+		replace_unicode(key, value)
+		
+	output_train_txt_file = "%s/training_text.replaced_unicode.p" % config.data_folder
+	output_test_txt_file = "%s/test_text.replaced_unicode.p" % config.data_folder
+	print("Saving replaced text data of the entire training set...")
+	with open(output_train_txt_file, "wb") as f:
+		pickle.dump(df_train_txt, f)
+	print("Saving replaced text data of the entire testing set...")
+	with open(output_test_txt_file, "wb") as f:
+		pickle.dump(df_test_txt, f)
--- Code/Preprocess/run.py
+++ Code/Preprocess/run.py
@@ -1,3 +1,5 @@
+from replace_text import replace_all
 from extract_pattern import extract_all
 
+replace_all()
 extract_all()
