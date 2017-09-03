0ebfcd81eeee6129a4a76a8585fda46e00401ec7
Janzen Liu <chengtsung_liu@yahoo.com>
2017-09-03 19:57:10 +0800

    add dump feature function and refactor codes

--- Code/Feat/genFeat_counting_feat.py
+++ Code/Feat/genFeat_counting_feat.py
@@ -6,38 +6,31 @@ import re
 import pickle
 import sys; sys.path.append("../"); sys.path.append("../Preprocess/")
 from param_config import config
-from reader import load_original_text
+from reader import load_original_text, load_stratified_kfold
+from feat_utils import dump_feat, dump_feats
 from unicode_dict import unicodes
 
 df_train_txt, df_test_txt = load_original_text()
+skf = load_stratified_kfold()
 
 def count_unicode_in_text(text, code_regex):
 	return len(re.findall(code_regex, text, re.UNICODE))
 
-def extract_unicode_count(code_regex, code_name):
+def extract_unicode_count(code_regex, dump=False):
 	feat_name = "count_of_unicode_%s" % code_regex[1:]
-	output_train_count_file = "%s/Raw/%s.train.p" % (config.feat_folder, feat_name)
-	output_test_count_file = "%s/Raw/%s.test.p" % (config.feat_folder, feat_name)
-
-	print("Generating feature %s (%s) for the entire training set..." % (feat_name, code_name))
 	df_train_txt[feat_name] = df_train_txt["Text"].apply(lambda x: count_unicode_in_text(x, code_regex))
-	with open(output_train_count_file, "wb") as f:
-		pickle.dump(df_train_txt[feat_name], f)	
-	print("Generating feature %s (%s) for the entire testing set..." % (feat_name, code_name))
 	df_test_txt[feat_name] = df_test_txt["Text"].apply(lambda x: count_unicode_in_text(x, code_regex))
-	with open(output_test_count_file, "wb") as f:
-		pickle.dump(df_test_txt[feat_name], f)
+	if dump:
+		dump_feat(df_train_txt, df_test_txt, feat_name)
 
 def extract_all():
+	############################
+	## get unicode count feat ##
+	############################
 	with open(config.unicode_cache_file, "rb") as f:
 		old_unicodes = pickle.load(f)
-
 	for key, value in unicodes.items():
-		if old_unicodes.get(key) == value:
-			print("Skip extracting counting feature for unicode %s (%s)" % (key[1:], value))
+		# skip if the counting feature of the unicode has been extracted
+		if key in old_unicodes.keys():
 			continue
-		extract_unicode_count(key, value)
-		print("Updating unicode %s (%s) in cache..." % (key[1:], value))
-		old_unicodes[key] = value
-		with open(config.unicode_cache_file, "wb") as f:
-			pickle.dump(old_unicodes, f)
+		extract_unicode_count(key, True)
--- Code/Feat/run.py
+++ Code/Feat/run.py
@@ -1,3 +1,8 @@
-from genFeat_counting_feat import extract_all
+import os
 
+# #### generate kfold
+# cmd = "python3 ./gen_kfold.py"
+# os.system(cmd)
+
+from genFeat_counting_feat import extract_all
 extract_all()
