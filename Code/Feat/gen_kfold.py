fab79bf7149098d567d94f61c4fa9af7da7a429a
Janzen Liu <chengtsung_liu@yahoo.com>
2017-09-08 00:50:07 +0800

    add documentation and clean codes

--- Code/Feat/gen_kfold.py
+++ Code/Feat/gen_kfold.py
@@ -6,15 +6,21 @@ from param_config import config
 from reader import load_original_variants
 from sklearn.model_selection import StratifiedKFold
 
-## load data
-df_train_var, df_test_var = load_original_variants()
-
-skf = [0]*config.n_runs
-for stratified_label,key in zip(["disease_class", "mutated_gene"], ["Class", "Gene"]):
-    for run in range(config.n_runs):
+'''
+generate stratified kfold with specific parameters
+@param:
+    n_runs, n_folds: number of runs and folds
+    label: column name indicating the target used for stratification
+    df: dataframe to produce kfold
+@return:
+    skf[i][j] indicates the indices for training and validation subset in the (j+1) fold in the (i+1)th run
+'''
+def gen_kfold(n_runs=config.n_runs, n_folds=config.n_folds, label, df):
+    skf = [0]*config.n_runs
+    for run in range(n_runs):
         random_seed = 2017 + 1000 * (run+1)
-        skf_helper = StratifiedKFold(n_splits=config.n_folds, shuffle=True, random_state=random_seed)
-        skf[run] = list(skf_helper.split(df_train_var.drop([key], axis=1), df_train_var[key]))
+        skf_helper = StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=random_seed)
+        skf[run] = list(skf_helper.split(df, df[label]))
         for fold, (trainInd, validInd) in enumerate(skf[run]):
             print("================================")
             print("Index for run: %s, fold: %s" % (run+1, fold+1))
@@ -22,5 +28,15 @@ for stratified_label,key in zip(["disease_class", "mutated_gene"], ["Class", "Ge
             print(trainInd[:10])
             print("Valid (num = %s)" % len(validInd))
             print(validInd[:10])
-    with open("%s/stratifiedKFold.%s.p" % (config.data_folder, stratified_label), "wb") as f:
-        pickle.dump(skf, f)
+    return skf
+
+if __name__ == "__main__":
+    old_skf = load_stratified_kfold()
+    if old_skf == "error" or len(old_skf) != config.n_runs or len(old_skf[0]) != config.n_folds:
+        ## load data
+        df_train_var, df_test_var = load_original_variants()
+
+        ## generate kfold using 'Class' and 'Gene' as stratification target and save them
+        for stratified_label,key in zip(["disease_class", "mutated_gene"], ["Class", "Gene"]):
+            with open("%s/stratifiedKFold.%s.p" % (config.data_folder, stratified_label), "wb") as f:
+                pickle.dump(gen_kfold(label=key, df=df_train_var), f)
--- Code/Preprocess/replace_text.py
+++ Code/Preprocess/replace_text.py
@@ -9,27 +9,39 @@ from param_config import config
 from reader import load_original_text
 from unicode_dict import unicodes
 
-df_train_txt, df_test_txt = load_original_text()
-
-def replace_unicode(code_regex, new_code):
+'''
+Replace a specific Unicode character with a specified piece of text.
+Note: The replacement happens on the global variable df_train_txt and df_test_txt, which should be improved
+@param:
+	code_regex: regular expression for the Unicode character
+	new_code: text used to replace the character
+	dfs: a list of dataframes with 'Text' column
+'''
+def replace_unicode(code_regex, new_code, dfs):
 	print("Replacing unicode %s (%s)..." % (code_regex[1:], new_code))
-	# print("Replacing unicode %s in the entire training set..." % new_code)
-	for index, row in df_train_txt.iterrows():
-		df_train_txt.set_value(index, 'Text', re.sub(code_regex, new_code, row['Text'], re.UNICODE))
-	# print("Replacing unicode %s in the entire testing set..." % new_code)
-	for index, row in df_test_txt.iterrows():
-		df_test_txt.set_value(index, 'Text', re.sub(code_regex, new_code, row['Text'], re.UNICODE))
+	for df in dfs:
+		for index, row in df.iterrows():
+			df.set_value(index, 'Text', re.sub(code_regex, new_code, row['Text'], re.UNICODE))
 
-def replace_all():
-	for key, value in unicodes.items():
-		replace_unicode(key, value)
+'''
+1. Perform replacement with all charcater/substitute specified in unicode_dict.py
+2. Save the replaced text to path specified inside the function
+@param:
+	dfs: a list of dataframes to perform replacement on
+	codes: a dictionary for code replacement
+'''
+def replace_all(dfs, codes):
+	for key, value in codes.items():
+		replace_unicode(key, value, dfs)
 
+
+if __name__ == "__main__":
+	df_train_txt, df_test_txt = load_original_text()
+	replace_all([df_train_txt, df_test_txt], unicodes)
 	output_train_txt_file = "%s/training_text.replaced_unicode.p" % config.data_folder
 	output_test_txt_file = "%s/test_text.replaced_unicode.p" % config.data_folder
-	print("Saving replaced text data...")
-	# print("Saving replaced text data of the entire training set...")
+	print("Saving text with speicial characters replaced...")
 	with open(output_train_txt_file, "wb") as f:
 		pickle.dump(df_train_txt, f)
-	# print("Saving replaced text data of the entire testing set...")
 	with open(output_test_txt_file, "wb") as f:
 		pickle.dump(df_test_txt, f)
