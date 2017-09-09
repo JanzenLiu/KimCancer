4497d6ffcec481f6f12a2c4d0f0830ddcf2e2f26
Janzen Liu <chengtsung_liu@yahoo.com>
2017-09-09 16:51:41 +0800

    add function to find all special characters

---
+++ Code/Preprocess/find_unicode.py
@@ -0,0 +1,37 @@
+import re
+import sys; sys.path.append('../')
+from reader import load_original_text
+
+normal_chars = [',', '.', '?', '!', ':', ';', 
+				'(', ')', '[', ']', '{', '}',
+				'>', '<', '=', '+', '-', '/',
+				'@', '#', '$', '%', '^', '&',
+				'*', '`', '~', '\'', '\"', '\\', '\t']
+
+def special_char_set_lct(text):
+	res = re.sub(r'\w', '', text)
+	res = res.replace(" ", "")
+	for char in normal_chars:
+		res = res.replace(char, '')
+	return set(res)
+
+def special_char_set_lkc(text):
+	return set(re.findall(r'[\u00ff-\uffff]', text))
+
+if __name__ == "main":
+	df_train_txt, df_test_txt = load_original_text()
+	train_special_chars = set()
+	test_special_chars = set()
+
+	for index, row in df_train_txt.iterrows():
+		# train_special_chars.update(special_char_set(row['Text']))
+		train_special_chars.update(special_char_set_lkc(row['Text']))
+	for index, row in df_test_txt.iterrows():
+		# test_special_chars.update(special_char_set(row['Text']))
+		test_special_chars.update(special_char_set_lkc(row['Text']))
+	special_chars = train_special_chars.copy()
+	special_chars.update(test_special_chars)
+
+	with open('special_chars', 'w') as f:
+		for char in special_chars:
+			f.write(char + '\n')
