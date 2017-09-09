35fa08c407678cb586a8d1cb1e4586ecca2f6814
Janzen Liu <chengtsung_liu@yahoo.com>
2017-09-10 03:08:19 +0800

    little update

---
+++ Code/EDA/find_special_chars.py
@@ -0,0 +1,37 @@
+import re
+import sys; sys.path.append('../')
+from param_config import config
+from reader import load_original_text
+from tools import save_words
+
+normal_chars = [',', '.', '?', '!', ':', ';', 
+				'(', ')', '[', ']', '{', '}',
+				'>', '<', '=', '+', '-', '/',
+				'@', '#', '$', '%', '^', '&',
+				'*', '`', '~', '\'', '\"', '\\', '\t']
+
+def special_char_set_lct(text):
+	res = re.sub(r'\w', '', text)
+	res = res.replace(" ", "")
+	for char in normal_chars:
+		res = res.replace(char, '')
+	return set(res)
+
+def special_char_set_lkc(text):
+	return set(re.findall(r'[\u00ff-\uffff]', text))
+
+if __name__ == "__main__":
+	df_train_txt, df_test_txt = load_original_text()
+	train_special_chars = set()
+	test_special_chars = set()
+
+	for index, row in df_train_txt.iterrows():
+		# train_special_chars_lct.update(special_char_set(row['Text']))
+		train_special_chars.update(special_char_set_lkc(row['Text']))
+	for index, row in df_test_txt.iterrows():
+		# test_special_chars_lct.update(special_char_set(row['Text']))
+		test_special_chars.update(special_char_set_lkc(row['Text']))
+	special_chars = train_special_chars.copy()
+	special_chars.update(test_special_chars)
+
+	save_words(special_chars, config.special_characters_path)
---
+++ Code/EDA/tokenizer.py
@@ -0,0 +1,43 @@
+import nltk
+import json
+import pickle
+import numpy as np
+import pandas as pd
+import sys; sys.path.append('../')
+from param_config import config
+from reader import load_processed_data, load_original_text
+from tools import seriesToJson
+
+def tokenize(doc):
+	tokenized_doc = list()
+	for sent in nltk.sent_tokenize(doc):
+		tokenized_doc.append(nltk.word_tokenize(sent))
+	return tokenized_doc
+
+def tokenize_df(df):
+	df['tokenized_text'] = df['Text'].map(tokenize)
+
+def dump_tokenized_json(df, path):
+	if not 'tokenized_text' in df.columns:
+		tokenize_df(df)
+	tokens_json = seriesToJson(df['tokenized_text'], str)
+	with open(path, 'w') as f:
+		json.dump(tokens_json, f, indent=2)
+
+def dump_tokenized_csv(df, path):
+	if not 'tokenized_text' in df.columns:
+		tokenize_df(df)
+	df[['ID', 'tokenized_text']].to_csv(path, index=False)
+
+def dump_tokenized_pickle(df, path):
+	if not 'tokenized_text' in df.columns:
+		tokenize_df(df)
+	with open(path, 'wb') as f:
+		pickle.dump(df['tokenized_pattern'].values, f)
+
+if __name__ == '__main__':
+	df_train_txt, df_test_txt = load_original_text()
+	dump_tokenized_json(df_train_txt, config.train_tokens_path)
+	dump_tokenized_json(df_test_txt, config.test_tokens_path)
+	
+
