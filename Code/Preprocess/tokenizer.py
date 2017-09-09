866ee6378a78865d166de0e7e71fa875d0e69eb4
JanzenLiu <chengtsung_liu@yahoo.com>
2017-09-10 00:51:24 +0800

    update .gitignore

--- Code/Preprocess/tokenizer.py
+++ Code/Preprocess/tokenizer.py
@@ -6,6 +6,7 @@ from reader import load_processed_data
 def tokenize(df):
 	docs = dict()
 	for index, row in df.iterrows():
+		print('processing %d' % index)
 		sents = list()
 		for sent in nltk.sent_tokenize(row['Text']):
 			sents.append(nltk.word_tokenize(sent))
