1e542147a5d32c807c1fbe5fbcf3be6aa09d5c8a
Janzen Liu <chengtsung_liu@yahoo.com>
2017-09-10 19:47:28 +0800

    update word_dict

--- Code/EDA/find_maps.py
+++ Code/EDA/find_maps.py
@@ -1,15 +1,30 @@
 import nltk
 
-def find_map(sent):
-	dic = dict()
-	if type(sent) == str:
-		sent = nltk.word_tokenize(sent)
-	for index, word in enumerate(sent):
-		if (index+3 < len(sent)) and (sent[index+1] == "(" and sent[index+3] == ")"):
-			if dic.get(word, None) == None:
-				dic[word] = list()
-			dic[word].append(sent[index+2])
-	return dic
+class WordMap():
+	def __init__(self):
+		self.dic = dict()
 
-# def find_map_doc(doc):
-# 	if
+	def map_sent(self, sent):
+		if type(sent) == str:
+			sent = nltk.word_tokenize(sent)
+		for index, word in enumerate(sent):
+			if (index+3 < len(sent)) and (sent[index+1] == "(" and sent[index+3] == ")") and (not sent[index+2].isdigit()):
+				if not word in self.dict:
+					self.dic[word] = set()
+				self.dic[word].add(sent[index+2])
+
+	def map_doc(self, doc):
+		for sent in doc:
+			self.map_sent(sent)
+
+	def map_docs(self, docs):
+		for doc in docs:
+			self.map_doc(doc)
+
+	def map_df(self, df, col_name='Text'):
+		self.map_docs(df[col_name].values)
+
+	def clear_map(self):
+		self.map = dict()
+
+
